5e2a879f8edffaf198284b97fe7060a8ad53efe9
weiruhan <23504121+weiruhan@users.noreply.github.com>
2025-08-02 23:39:41 -0400

    在tradeSPOT中加入设置持仓方向功能

--- okx/app/trade/tradeSPOT/_base.py
+++ okx/app/trade/tradeSPOT/_base.py
@@ -13,6 +13,7 @@ class TradeBase():
             timezone: str = 'Asia/Shanghai',
             account=None,
             market=None,
+            posmode: str = 'long_short_mode',
             proxies={},
             proxy_host: str = None,
     ):
@@ -29,6 +30,16 @@ class TradeBase():
                                       proxy_host=proxy_host)
         else:
             self._market = market
+            
+        # 设置持仓方向
+        set_position_mode_result = self._account.set_position_mode(
+            posMode=posmode
+        )
+        if set_position_mode_result['code'] == '0':
+            print('设置持仓方式为双向持仓成功' if posmode == 'long_short_mode' else '设置持仓方式为单向持仓成功')
+        else:
+            print("设置持仓方式失败。请先取消所有未成交订单，平掉所有持仓，并停止交易机器人。")
+
 
         # 时区
         self.timezone = timezone
